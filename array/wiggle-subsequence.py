211000493019cd32c23a660efe3359dd7143c512
Yujia Zhou <59751647+Stoneykk@users.noreply.github.com>
2025-09-12 18:57:59 -0700

    LC-wiggle-subsequence [Runtime: 0 ms; Memory: 17.9 MB]

---
+++ array/wiggle-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 1
+        prediff = 0
+        curdiff = 0
+        result = 1
+
+        for i in range(len(nums)-1):
+            curdiff = nums[i+1] - nums[i]
+            if (prediff <= 0 and curdiff > 0) or (prediff >= 0 and curdiff < 0):
+                result += 1
+                prediff = curdiff
+        return result
