6be8c0b376bba664f25b07f6989128f6e6fbf9cd
Yujia Zhou <59751647+Stoneykk@users.noreply.github.com>
2025-10-03 17:19:16 -0700

    LC-valid-triangle-number [Runtime: 418 ms; Memory: 17.7 MB]

---
+++ two-pointers/valid-triangle-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        ans = 0
+        for k in range(2, len(nums)):
+            c = nums[k]
+            i = 0
+            j = k - 1
+            while i < j:
+                if nums[i] + nums[j] > c:
+                    ans += j - i
+                    j -= 1
+                else:
+                    i += 1
+        return ans
