ed38ef92f11d2721a833fc038f9b0ce1ea283fad
Yujia Zhou <59751647+Stoneykk@users.noreply.github.com>
2025-10-03 17:53:13 -0700

    LC-watering-plants-ii [Runtime: N/A; Memory: N/A]

---
+++ array/watering-plants-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minimumRefill(self, plants: List[int], capacityA: int, capacityB: int) -> int:
+        ca, cb = capacityA, capacityB
+        a, b = 0, len(plants)-1
+        ans = 0
+        while a < b:
+            # Alice
+            if ca > plants[a]:
+                ca -= plants[a]
+                a += 1
+            else: # ca <= plants[a]
+                ca = capacityA
+                ans += 1
+                ca -= plants[a]
+                a += 1
+            # Bob
+            if cb > plants[b]:
+                cb -= plants[b]
+                b -= 1
+            else:
+                cb = capacityB
+                ans += 1
+                cb -= plants[b]
+                b -= 1
+        if a == b and max(ca, cb) < plants[a]:
+            ans += 1
+        
+        return ans
